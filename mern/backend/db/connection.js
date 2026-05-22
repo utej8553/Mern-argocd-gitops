1caf146ad1ab5c707317e75124bb46ac33a61af8
Utej <91786744+utej8553@users.noreply.github.com>
2026-05-22 16:35:18 +0530

    changes urls

--- mern/backend/db/connection.js
+++ mern/backend/db/connection.js
@@ -1,6 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const URI = "mongodb://mongodb:27017";
+const URI = process.env.MONGO_URI || "mongodb://mongodb:27017";
 const client = new MongoClient(URI, {
   serverApi: {
     version: ServerApiVersion.v1,
--- mern/backend/server.js
+++ mern/backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
 
-const PORT = process.env.PORT || 5050;
+const PORT = 5000;
 const app = express();
 
 app.use(cors());
